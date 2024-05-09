1e746abd412c3641d0550b7ccd7031a0eaf7a460
OdashevBexruzbek <bexruzbekodashev@gmail.com>
2024-05-09 18:04:09 +0500

    6 homework

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <>
+      <div>
+        <h1>Welcome About Page</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quo
+          repudiandae atque dolorum. Architecto similique ab aliquid, quibusdam
+          exercitationem magnam?
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default About;
